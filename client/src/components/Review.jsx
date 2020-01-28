383710a9b3ad2f960d7a5c61809c27c4d101263d
Adam <gienappa@gmail.com>
2020-01-27 22:09:52 -0800

    initial component render structure complete.
    app.jsx - all get methods added

--- client/src/components/Review.jsx
+++ client/src/components/Review.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 
 const Review = (props) => {
+  const { info } = props.info;
   return (
     <div>
-
+      <div className="review=body">
+        <div>(stars)</div>
+        <div>{info.date}</div>
+        <h5>{info.title.toUpperCase()}</h5>
+        <div>{info.body}</div>
+        <div>(I recommend...)</div>
+        <div>
+          <span>{info.name}</span>
+          <span>(verified purchaser)</span>
+        </div>
+      </div>
+      <div className="review-replies">
+        <span>Was this review helpful?</span>
+        <span>Yes</span> <span>({info.helpfulYes})</span>
+        <span>No</span> <span>({info.helpfulNo})</span>
+      </div>
     </div>
   );
 };
