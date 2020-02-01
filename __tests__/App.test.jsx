4854006e81d3d00174dfd03cabd8b4d223a87ada
Adam <gienappa@gmail.com>
2020-02-01 14:10:46 -0800

    App.test.jsx filled in with two initial unit tests

--- __tests__/App.test.jsx
+++ __tests__/App.test.jsx
@@ -3,10 +3,16 @@ import React from 'react';
 import { mount, shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import App from '../client/src/components/App';
+import Review from '../client/src/components/Review';
 
 describe('<App /> Unit Tests', () => {
   it('should render the app component on the screen', () => {
     const wrapper = shallow(<App />);
     expect(wrapper).toExist();
+  });
+
+  it('renders two Review components', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Review)).to.have.length(2);
   });
 });
