14424fac495691909d4fb4b533876abb07e91674
Adam <gienappa@gmail.com>
2020-02-01 13:14:23 -0800

    testing setup maybe complete?

---
+++ tests/App.test.jsx
@@ -0,0 +1,10 @@
+/* eslint-env jest */
+import React from 'react';
+import { shallow } from 'enzyme';
+import axios from 'axios';
+import App from '../client/src/components/App';
+
+describe('<App /> Unit Tests', () => {
+
+  
+});
