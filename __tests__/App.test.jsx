e51e54b77c3255ef066a435eb7932a42327324e8
Adam <gienappa@gmail.com>
2020-02-01 16:26:45 -0800

    App.test.jsx - three tests working, 34% coverage

--- __tests__/App.test.jsx
+++ __tests__/App.test.jsx
@@ -2,17 +2,52 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 // import toJson from 'enzyme-to-json';
+// import axios from 'axios';
 import App from '../client/src/components/App';
-import Review from '../client/src/components/Review';
 
 describe('<App /> Unit Tests', () => {
+  jest.mock('axios', () => {
+    const data = {
+      data: [
+        {
+          rating: 3,
+          title: 'Best shoes',
+          recommended: true,
+          name: 'Selena.Doyle96',
+          verified: false,
+        },
+        {
+          rating: 1,
+          title: 'Beware!',
+          recommended: false,
+          name: 'Belle.Adams',
+          verified: true,
+        },
+      ],
+    };
+    return {
+      get: jest.fn(() => Promise.resolve(data)),
+    };
+  });
+
   it('should render the app component on the screen', () => {
     const wrapper = shallow(<App />);
     expect(wrapper).toExist();
   });
 
-  it('renders two Review components', () => {
+  it('should invoke getReviewsSummary on componentDidMount', () => {
     const wrapper = shallow(<App />);
-    expect(wrapper.find(Review)).to.have.length(2);
+    const mock = jest.fn();
+    wrapper.instance().getReviewsSummary = mock;
+    wrapper.instance().forceUpdate();
+    wrapper
+      .instance()
+      .componentDidMount();
+    expect(mock).toHaveBeenCalled();
+  });
+
+  it('should render five slider components', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper).toContainMatchingElements(5, '.star-bar');
   });
 });
